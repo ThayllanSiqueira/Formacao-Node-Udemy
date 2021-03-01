f87177079f594f3208be4fdfd00801b198f3447a
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-03-01 13:44:56 -0300

    Iniciando modulo-5

---
+++ modulo-5_projeto-1_plataforma-de-perguntas-e-respostas/guiaperguntas/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.send("Bem vindo ao site!");
+});
+
+app.listen(8080, () => { console.log("App Rodando..."); });
