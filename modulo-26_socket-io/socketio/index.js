f349bc421aece40ec3199166707d5a46ec14ae6e
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-04-07 13:28:48 -0300

    modulo-26 finalizado

--- modulo-26_socket-io/socketio/index.js
+++ modulo-26_socket-io/socketio/index.js
@@ -2,3 +2,30 @@ const express = require("express");
 const app = express();
 
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on('connection', (socket) => {
+
+    socket.on("disconnect", () => {
+        console.log("X desconectou: " + socket.id)
+    })
+
+    socket.on("boasvindas", (data) => {
+        console.log(data);
+    })
+    socket.on("palavra", (data) => {
+        console.log(data);
+        socket.emit("resultado", data + " Faz supino com minha rola.")
+    })
+
+})
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+http.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+})
